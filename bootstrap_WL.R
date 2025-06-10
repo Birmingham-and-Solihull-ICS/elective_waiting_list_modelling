44744eaf60f327153f207ba7a1e4e48cbd1b5b30
Chris Mainey <c.mainey1@nhs.net>
2025-06-10 11:40:04 +0100

    fixed referencing in monte function carlo script

--- bootstrap_WL.R
+++ bootstrap_WL.R
@@ -6,32 +6,33 @@ bsol_simulate_WL <-
     
     
     # Make sure date columns are right format
-    if(!is(object = .data$start_date, "Date")){
-      .data$start_date <- as.Date(.data$start_date, format = "%d/%m/%Y")
+    if (!is(object = .data[[start_date_name]], "Date")) {
+      .data[[start_date_name]] <- as.Date(.data[[start_date_name]], format = "%d/%m/%Y")
     }
-    
-    if(!is(object = .data$end_date, "Date")){
-      .data$end_date <- as.Date(.data$end_date, format = "%d/%m/%Y")
+
+    if (!is(object = .data[[end_date_name]], "Date")) {
+      .data[[end_date_name]] <- as.Date(.data[[end_date_name]], format = "%d/%m/%Y")
     }
-    
+
     
     # rows to iterate over in control table
     sim_period_n <- nrow(.data)
     
-    if(!missing(seed)){set.seed(seed)}
+    if (!missing(seed)) {set.seed(seed)}
     
+    sims <- list(NROW(.data))
     
     seq_periods <- 
     
-    for(i in seq(1, sim_period_n) ){
-      if(i == 1){
+    for (i in seq(1, sim_period_n)) {
+      if (i == 1) {
         
         inpt <- unlist(.data[1,])
         
         # handle starting waiting list
-        if(starting_wl > 0){ #Dump current waiting list figure into the day before the modelling period
+        if (starting_wl > 0) { #Dump current waiting list figure into the day before the modelling period
           #WL warm-up
-          wl_date <- as.POSIXlt(.data$start_date)
+          wl_date <- as.POSIXlt(.data[[start_date_name]][i])
           wl_date$mon <- wl_date$mon - 1
           wl_date <- as.character(wl_date)
           
@@ -45,48 +46,45 @@ bsol_simulate_WL <-
           #                                            demand = waiting_list_current,
           #                                            capacity = waiting_list_current * 0.8)
           # 
-          current_wl <- data.frame(Referral = rep(as.Date(wl_date),starting_wl) 
+          current_wl <- data.frame(Referral = rep(as.Date(wl_date), starting_wl) 
                                    , Removal = rep(as.Date(NA), starting_wl)
           )
       
      
       
-          sim_1 <- NHSRwaitinglist::wl_simulator(as.Date(inpt[start_date_name], format = "%d/%m/%Y")
-                                                 , as.Date(inpt[end_date_name], format = "%d/%m/%Y")
-                                                 , as.integer(inpt[adds_name])
-                                                 , as.integer(inpt[removes_name])
+          sims[[i]] <- NHSRwaitinglist::wl_simulator(as.Date(.data[[start_date_name]][i], format = "%d/%m/%Y")
+                                                , as.Date(.data[[end_date_name]][i], format = "%d/%m/%Y")
+                                                , as.integer(.data[[adds_name]][i])
+                                                , as.integer(.data[[removes_name]][i])
                                                  , waiting_list = current_wl)
         } else {
           
-          sim_1 <- NHSRwaitinglist::wl_simulator(as.Date(inpt[start_date_name], format = "%d/%m/%Y")
-                                                 , as.Date(inpt[end_date_name], format = "%d/%m/%Y")
-                                                 , as.integer(inpt[adds_name])
-                                                 , as.integer(inpt[removes_name]))
+          sims[[i]] <- NHSRwaitinglist::wl_simulator(as.Date(.data[[start_date_name]][i], format = "%d/%m/%Y")
+                                                 , as.Date(.data[[end_date_name]][i], format = "%d/%m/%Y")
+                                                 , as.integer(.data[[adds_name]][i])
+                                                 , as.integer(.data[[removes_name]][i])
+          )
         }
     } else {
       
-      if(!missing(seed)){set.seed(seed)}  
+      if (!missing(seed)) {set.seed(seed)}  
         
-      inpt <- unlist(.data[i,])
-        
-      eval(
-        call("<-"
-             , as.name(paste0("sim_",as.character(i)))
-             , NHSRwaitinglist::wl_simulator(as.Date(inpt[start_date_name], format = "%d/%m/%Y")
-                            , as.Date(inpt[end_date_name], format = "%d/%m/%Y")
-                            , as.integer(inpt[adds_name])
-                            , as.integer(inpt[removes_name])
-                            ,  waiting_list = get(paste0("sim_", as.character(i-1))))
-              )
-        )
+      
+          sims[[i]] <- NHSRwaitinglist::wl_simulator(as.Date(.data[[start_date_name]][i], format = "%d/%m/%Y")
+                            , as.Date(.data[[end_date_name]][i], format = "%d/%m/%Y")
+                            , as.integer(.data[[adds_name]][i])
+                            , as.integer(.data[[removes_name]][i])
+                            ,  waiting_list = sims[[i - 1]]
+          )
+   
             
       
       } 
     }
     
-    return(NHSRwaitinglist::wl_queue_size(get(paste0("sim_", as.character(i)))))
+    return(NHSRwaitinglist::wl_queue_size(sims[[NROW(.data)]]))
     
-  }
+}
 
 
 
